403b09b1175cd8ccc12b6d8ea79892aa2f0cb692
Shiva <shivateja@yahoo.com>
2022-02-19 04:34:37 +0530

    updated

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,6 +1,6 @@
 def call() {
     try {
-        File file = new File("${WORKSPACE}/pom.xml");
+        File file = new File("${WORKSPACE}/pom*");
         FileReader fr = new FileReader(file);
         sh 'mvn clean package'
     }
