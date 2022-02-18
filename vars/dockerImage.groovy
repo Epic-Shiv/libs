5b5faa75b901e364821742b39265459362736a1a
Shiva <shivateja@yahoo.com>
2022-02-19 04:32:36 +0530

    updated

--- vars/dockerImage.groovy
+++ vars/dockerImage.groovy
@@ -1,6 +1,6 @@
 def build(Map config) {
     try {
-        File file = new File("${WORKSPACE}/Docker*");
+        File file = new File("${WORKSPACE}/Dockerfile*");
         FileReader fr = new FileReader(file);
         sh "docker image build -t ${config.imageName} ."
     }
@@ -10,7 +10,7 @@ def build(Map config) {
 }
 def push(Map config) {
     try {
-        File file = new File("${WORKSPACE}/Docker*");
+        File file = new File("${WORKSPACE}/Dockerfile*");
         FileReader fr = new FileReader(file);
         sh "docker push ${config.imageName}"
     }
